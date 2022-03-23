26d3823e6c63819bab981d12f4450b7548df553c
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-03-23 16:07:54 +0100

    Try to fix props value for login/logout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
 
-export const Layout: React.FC = ({children}) => {
+export const Layout: React.FC = (props: { cookie: string}, {children}) => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -10,8 +10,11 @@ export const Layout: React.FC = ({children}) => {
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/genre">Genre</Nav.Link>
           <Nav.Link href="/platform">Platform</Nav.Link>
-          <Nav.Link href="/api/auth/login">Login/SignUp</Nav.Link>
-          <Nav.Link href="/api/auth/logout">Logout</Nav.Link>
+            {props.cookie ? (
+              <Nav.Link href="/api/auth/logout">Logout</Nav.Link>
+            ) : (
+            <Nav.Link href="/api/auth/login">Login/SignUp</Nav.Link>
+            )}
           <Nav.Link href="/api/auth/me">Profil</Nav.Link>
         </Nav>
         </Container>
